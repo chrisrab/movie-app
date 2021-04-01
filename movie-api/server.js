527da86950cf731b27fbc502463fab66ac53b3d1
Christopher Rabone <christopherrabone1@gmail.com>
2021-04-01 16:24:23 +0100

    sending to warren for review

--- movie-api/server.js
+++ movie-api/server.js
@@ -8,9 +8,15 @@ const app = express();
 
 var jsonParser = bodyParser.json();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const getMovie = id => {
+  return movies.find(movie => {
+    return movie.id === id;
+  });
+};
 
 app.use(cors());
+
+app.use(jsonParser);
 
 app.get('/movies', (req, res) => {
   res.send(movies);
@@ -19,7 +25,7 @@ app.get('/movies', (req, res) => {
 app.get('/movies/:id', (req, res) => {
   const id = req.params.id;
 
-  movie(id);
+  const movie = getMovie(id);
 
   if (!movie) {
     res.status(404).send('Not found');
@@ -29,16 +35,34 @@ app.get('/movies/:id', (req, res) => {
   res.send(movie);
 });
 
-app.post('/movies/:id/reviews', urlencodedParser, (req, res) => {
-  console.log(req.params.id);
-  console.log(req.body.review);
+app.post('/movies/:id/reviews', (req, res) => {
+  //urlencoder or jsonParser\
+
+  const id = req.params.id;
+  const movie = getMovie(id);
+  //console.log(req.body, 'body');
+  const review = req.body;
+  console.log(req.body, 'review here');
+  movie.reviews.push(review);
+  console.log(movie);
+
+  //console.log(req.params.id);
+  res.status(200).send(movie);
 });
 
-const movie = id => {
-  movies.find(movie => {
-    return movie.id === id;
-  });
-};
+app.get('/movies/:id/reviews', (req, res) => {
+  const id = req.params.id;
+
+  const movie = getMovie(id);
+  const reviews = movie.reviews;
+
+  if (!movie) {
+    res.status(404).send('Not found');
+    return;
+  }
+
+  res.send(reviews);
+});
 
 app.listen(config.PORT, () => {
   console.log(`App is running at port http://localhost:${config.PORT}`);
--- movie-client/src/components/Description.js
+++ movie-client/src/components/Description.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { useState } from 'react';
 
 function Description({ movies }) {
+  //const [movie, setMovie] = useState([])
   const { id } = useParams();
-  const anyMovie = movies.filter(movie => movie.id == id);
+  const anyMovie = movies.filter(movie => movie.id === id);
+  //const [revs, setRevs] = useState([]);
 
   const movie = anyMovie[0];
+  console.log(movie);
 
   if (!movie) {
     return <h2>Movie Not Found</h2>;
@@ -13,16 +17,73 @@ function Description({ movies }) {
 
   const { title, image, reviews, genre, description } = movie;
 
+  function refreshPage() {
+    const reload = window.location.reload();
+    return reload;
+  }
+
+  function renderReviews(reviews) {
+    let getScores = reviews.map(function(e) {
+      return parseInt(e.score);
+    });
+
+    function average(array) {
+      return array.reduce((a, b) => a + b, 0) / array.length;
+    }
+
+    let reviewArray = reviews.map(function(e) {
+      return e.review;
+    });
+
+    const averageScoreNotFound = <h3>There are no reviews yet</h3>;
+
+    const averageScoreFound = <h3> The average user review score for this film is: {average(getScores)}/10</h3>;
+
+    return (
+      <div>
+        {reviews.length > 0 ? averageScoreFound : averageScoreNotFound}
+        <h2>These are the most recent reviews: </h2>
+        <ul>
+          {reviewArray.map(review => (
+            <li key={review}>"{review}"</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let review = event.target[0].value;
+    let score = event.target[1].value;
+
+    const payload = { review, score };
+
+    fetch(`http://localhost:3000/movies/${id}/reviews`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(payload)
+    })
+      .then(response => response.json())
+      .then(movie => {
+        console.log(movie); // Waz, is there a way to get the info that is returned out from here so i  can use this to render my reviews
+      });
+    refreshPage(); //Warren, I know this is disgusting amd goes against the whole point of react ha
+    //, I know there must be a really easy way to do this, i've tried quite a few!
+  }
+
   return (
     <div>
       <h2>{title}</h2>
       <img src={image} alt={title} style={bigImageStyle} />
       <h3>{genre}</h3>
       <p>{description}</p>
-      <form method="POST" action="http://localhost:3000/movies/:id/reviews">
+      <form onSubmit={handleSubmit}>
         <input type="text" name="review" placeholder="Write review here..."></input>
+        <input type="number" name="score" placeholder="Review score out of 10"></input>
         <input type="submit" value="submit"></input>
       </form>
+      <div>{renderReviews(reviews)}</div>
     </div>
   );
 }
@@ -33,3 +94,5 @@ const bigImageStyle = {
 };
 
 export default Description;
+
+//method="POST" action="http://localhost:3000/movies/godzilla/reviews"
